b2d719031d057f7b5791a31261d8b95701b6d825
Animesh <animeshmane@gmail.com>
2018-05-10 21:17:58 -0400

    client template added

---
+++ src/main/java/com/example/myApp/repositories/HelloRepository.java
@@ -0,0 +1,6 @@
+package com.example.myApp.repositories;
+import org.springframework.data.repository.CrudRepository;
+import com.example.myApp.Hello;
+public interface HelloRepository extends CrudRepository<Hello, Integer>{
+
+}
