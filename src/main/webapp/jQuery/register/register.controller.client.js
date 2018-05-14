99c648e8124b78f2b6697b959ea7991d143ba694
Animesh <animeshmane@gmail.com>
2018-05-14 10:26:39 -0400

    Bonus task for Registration and Password Validation implemented

--- src/main/webapp/jQuery/register/register.controller.client.js
+++ src/main/webapp/jQuery/register/register.controller.client.js
@@ -17,16 +17,25 @@ $( document ).ready(function() {
 	 {
 		var userName = $("#username").val();
 		
+		if (userName == ""){
+			$('#submitUser').attr('disabled', true);
+			alert("Registration unsuccessful!! Username cannot be null.Please try other user name!");
+			alert("Submit button disabled due to unsuccessful registration. Please reload page to register again!");
+		}
+		
+		else{
 	    
 		 var response = userService.findUserbyUserName(userName).then(function(results){
 			    var respo = results;
 			    if(respo.exists == 'true')
 			    	{
 			    			$('#submitUser').attr('disabled', true);
-			    			alert(" User exists. Submit button is Disabled. Please reload page and try other username");
+			    			alert("Registration unsuccessful!! Username already exists.Please try other user name!");
+			    			alert("Submit button disabled due to unsuccessful registration. Please reload page to register again!");
 			    	}
 			    
 		 });
+		 }
 		 
 		
 		 		 
@@ -37,7 +46,32 @@ $( document ).ready(function() {
 		 var password = $("#password").val();
 		 var userN = $("#username").val();
 		 var user = new User(userN,password,null,null,null,null,null,null);
-		 var newUser = userService.register(user)
+		 if (confirmPassword != password){
+			 if (confirmPassword == ""){
+				 alert("Password Validation failed.Confirm Password field is empty!")
+				 alert("Registration unsuccessful!! Confirm Password field empty.Please reload page to register again!")
+			 }
+			 
+			 else if (password == ""){
+				 alert("Password Validation failed.Password field is empty!")
+				 alert("Registration unsuccessful!! Password field empty. Please reload page to register again!")
+			 }
+			 
+			 else {
+				 alert("Password Validation failed. Passwords do not match!")
+				 alert("Registration unsuccessful!! Passwords do not match .Please reload page to register again!");
+			 }
+
+		 }
+		 
+		 
+		 else{
+			 var newUser = userService.register(user)
+			 alert("Successfully Registered!!!!")
+			 $("#username").val('');
+			 $("#password").val('');
+			 $("#confirm-password").val('');
+		 }
 	 }
 	 
 });
