e116d37ade80a71d491aeed8a0ab2593cad5cb32
Animesh <animeshmane@gmail.com>
2018-05-20 14:40:25 -0400

    Delete Course Implemented

--- src/main/java/com/example/myApp/webdev/services/CourseService.java
+++ src/main/java/com/example/myApp/webdev/services/CourseService.java
@@ -6,7 +6,9 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,5 +33,11 @@ public class CourseService {
 	public Course createUser(@RequestBody Course course) {
 		course.setCreated(new Date());
 		return courseRepository.save(course);
+	}
+	
+	@DeleteMapping("/api/course/{courseId}")
+	public void deleteUser(@PathVariable("courseId") int id) {
+		System.out.println("inside delete course");
+		courseRepository.deleteById(id);
 	}
 }
