c96bfbba7c75000ec707b24eebf4c5b88a3cc828
Animesh <animeshmane@gmail.com>
2018-05-30 22:43:14 -0400

    deploying on Heroku

--- src/main/java/com/example/myApp/webdev/repositories/WidgetRepository.java
+++ src/main/java/com/example/myApp/webdev/repositories/WidgetRepository.java
@@ -10,6 +10,7 @@ import com.example.myApp.webdev.models.Topic;
 import com.example.myApp.webdev.models.Widget;
 
 
+
 public interface WidgetRepository extends CrudRepository<Widget,Integer> {
 	@Query(value = "Select * from widget where topic_id =?1 order By widgetorder ", nativeQuery = true)
 	public List<Widget>findAllWidgetsForTopic(@Param("topicid") int topicid);
