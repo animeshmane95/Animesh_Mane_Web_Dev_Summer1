196e619bbf0fbeb6b04eb03b46ccbe62aba1551f
Animesh <animeshmane@gmail.com>
2018-06-06 01:13:50 -0400

    delete assignment executed

--- src/main/java/com/example/myApp/webdev/services/AssignmentService.java
+++ src/main/java/com/example/myApp/webdev/services/AssignmentService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,5 +56,11 @@ public class AssignmentService {
 		assignment.setTopic(topic.get());
 		return assignmentRepository.save(assignment);
 	}
+	
+
+	@DeleteMapping("/api/delete/assignment/{aid}")
+	public void deleteAssignment(@PathVariable("aid") int widgetId) {
+		assignmentRepository.deleteById(widgetId);	
+	}	
 	
 }
